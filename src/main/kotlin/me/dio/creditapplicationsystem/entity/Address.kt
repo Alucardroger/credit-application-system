2060ac54dbd2af2b72c32842860e3b09aefc6889
Alucardroger <140555450+Alucardroger@users.noreply.github.com>
2024-02-25 21:36:21 -0300

    Update

--- src/main/kotlin/me/dio/creditapplicationsystem/entity/Address.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/entity/Address.kt
@@ -1,12 +1,13 @@
 package me.dio.creditapplicationsystem.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.Embeddable
 import jakarta.persistence.Embedded
 
 @Embeddable
 data class Address (
-    var zipCode: String = "",
-    var street: String = ""
+    @Column(nullable = false) var zipCode: String = "",
+    @Column(nullable = false) var street: String = ""
 )
 
 
--- src/main/kotlin/me/dio/creditapplicationsystem/entity/Customer.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/entity/Customer.kt
@@ -3,16 +3,17 @@ package me.dio.creditapplicationsystem.entity
 import jakarta.persistence.*
 
 @Entity
-@Table(name = "Client")
+//@Table(name = "Client")
 data class Customer(
     @Column(nullable = false) var firstname: String = "",
     @Column(nullable = false) var lastname: String = "",
-    @Column(nullable = false) var email: String = "",
+    @Column(nullable = false, unique = true) var email: String = "",
+    @Column(nullable = false, unique = true) val cpf:String,
     @Column(nullable = false) var password: String = "",
-    @Column(nullable = false) val cpf:String,
     @Column(nullable = false) var address: Address = Address(),
     @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY,
-        cascade = arrayOf( CascadeType.REMOVE,
-            CascadeType.PERSIST), mappedBy = "customer") var credits: List<Credit> = mutableListOf(),
+        cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST),
+        mappedBy = "customer")
+    var credits: List<Credit> = mutableListOf(),
     @Column(nullable = false) @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null
 )
